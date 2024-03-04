1fd7275f06cd594bf7c6e59f92530a099400edd3
Sydus1 <j.sierraa@uniandes.edu.co>
2024-03-03 23:42:51 -0500

    Weapons Switch

---
+++ FPS/Assets/Scripts/Weapon/WeaponLogic.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLogic : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public GameObject bullet;
+
+    public float shotForce = 1500f;
+    public float shotRate = 0.5f;
+
+    private float shotRateTime = 0;
+
+    private AudioSource audioSource;
+
+    public AudioClip shotSound;
+
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Time.time > shotRateTime && GameManager.Instance.gunAmmo > 0)
+            {
+                if (audioSource != null)
+                {
+                    audioSource.PlayOneShot(shotSound);
+                }
+
+
+                GameManager.Instance.gunAmmo--;
+
+                GameObject newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+
+                shotRateTime = Time.time + shotRate;
+
+                Destroy(newBullet, 5);
+            }
+
+            
+        }
+    }
+}
